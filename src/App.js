71db6773e968e0b01d119f2cb248fe6ccec9e7bb
rafaelBPorto <rafaelbporto88@gmail.com>
2022-09-15 14:43:26 -0300

    Componentizacao

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Corpo from "./Corpo";
+import FundoMobile from "./FundoMobile";
+import NavBar from "./NavBar";
+
+export default function App() {
+    return (
+        <div>
+
+            <NavBar />
+            <Corpo />
+            <FundoMobile />
+    
+        </div>
+    )
+}
---
+++ src/Esquerda.js
@@ -0,0 +1,13 @@
+import Post from "./Post";
+import Stories from "./Stories";
+
+export default function Esquerda() {
+    return (
+        <div class="esquerda">
+
+            <Stories />
+            <Post />
+        </div>
+
+    )
+}
