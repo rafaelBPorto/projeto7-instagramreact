d67f5db593ad6dc4f911bce59370e21f14da6966
rafaelBPorto <rafaelbporto88@gmail.com>
2022-09-17 18:12:21 -0300

    Stories com map

--- src/Stories.js
+++ src/Stories.js
@@ -14,14 +14,14 @@ function Story(props) {
 export default function Stories() {
 
     const stories = [
-        {usuario: "9gag", imagem:"assets/img/9gag.svg" },
-        {usuario: "meowed" , imagem:"assets/img/meowed.svg" },
-        {usuario: "barked", imagem: "assets/img/barked.svg"},
-        {usuario: "nathanwpylestrangeplanet", imagem: "assets/img/nathanwpylestrangeplanet.svg"},
-        {usuario: "wawawicomics", imagem: "assets/img/wawawicomics.svg"},
-        {usuario: "respondeai", imagem: "assets/img/respondeai.svg"},
-        {usuario: "filomoderna", imagem: "assets/img/filomoderna.svg"},
-        {usuario: "memeriagourmet", imagem: "assets/img/memeriagourmet.svg"}
+        {usuario: "9gag", imagem: "./assets/img/9gag.svg" },
+        {usuario: "meowed" , imagem: "./assets/img/meowed.svg" },
+        {usuario: "barked", imagem: "./assets/img/barked.svg"},
+        {usuario: "nathanwpylestrangeplanet", imagem: "./assets/img/nathanwpylestrangeplanet.svg"},
+        {usuario: "wawawicomics", imagem: "./assets/img/wawawicomics.svg"},
+        {usuario: "respondeai", imagem: "./assets/img/respondeai.svg"},
+        {usuario: "filomoderna", imagem: "./assets/img/filomoderna.svg"},
+        {usuario: "memeriagourmet", imagem: "./assets/img/memeriagourmet.svg"}
 
     ]
     return (
