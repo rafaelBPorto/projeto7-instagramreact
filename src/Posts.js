2a6bf3757d10db595e20c815009a2c3703a99432
rafaelBPorto <rafaelbporto88@gmail.com>
2022-09-17 19:09:13 -0300

    Sugestoes com map

--- src/Posts.js
+++ src/Posts.js
@@ -3,7 +3,7 @@ function Post(props) {
         <div className="post">
             <div className="topo">
                 <div className="usuario">
-                    <img src={props.postImgUsuario}/>
+                    <img src={props.postImgUsuario} />
                     {props.postUsuario}
                 </div>
                 <div className="acoes">
@@ -51,9 +51,9 @@ export default function Posts() {
 
         {
             postUsuario: "barked",
-            postImgUsuario: "assets/img/barked.svg",
-            postImg: "assets/img/dog.svg",
-            curtidaUsuario: "assets/img/adorable_animals.svg",
+            postImgUsuario: "./assets/img/barked.svg",
+            postImg: "./assets/img/dog.svg",
+            curtidaUsuario: "./assets/img/adorable_animals.svg",
             curtidaImg: "adorable_animals",
             curtidaQuantidade: 99159
         },
@@ -72,75 +72,3 @@ export default function Posts() {
         </div>
     )
 }
-
-{/* <div className="post">
-<div className="topo">
-    <div className="usuario">
-        <img src="assets/img/meowed.svg" />
-        meowed
-    </div>
-    <div className="acoes">
-        <ion-icon name="ellipsis-horizontal"></ion-icon>
-    </div>
-</div>
-
-<div className="conteudo">
-    <img src="assets/img/gato-telefone.svg" />
-</div>
-
-<div className="fundo">
-    <div className="acoes">
-        <div>
-            <ion-icon name="heart-outline"></ion-icon>
-            <ion-icon name="chatbubble-outline"></ion-icon>
-            <ion-icon name="paper-plane-outline"></ion-icon>
-        </div>
-        <div>
-            <ion-icon name="bookmark-outline"></ion-icon>
-        </div>
-    </div>
-
-    <div className="curtidas">
-        <img src="assets/img/respondeai.svg" />
-        <div className="texto">
-            Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-        </div>
-    </div>
-</div>
-</div>
-
-<div className="post">
-<div className="topo">
-    <div className="usuario">
-        <img src="assets/img/barked.svg" />
-        barked
-    </div>
-    <div className="acoes">
-        <ion-icon name="ellipsis-horizontal"></ion-icon>
-    </div>
-</div>
-
-<div className="conteudo">
-    <img src="assets/img/dog.svg" />
-</div>
-
-<div className="fundo">
-    <div className="acoes">
-        <div>
-            <ion-icon name="heart-outline"></ion-icon>
-            <ion-icon name="chatbubble-outline"></ion-icon>
-            <ion-icon name="paper-plane-outline"></ion-icon>
-        </div>
-        <div>
-            <ion-icon name="bookmark-outline"></ion-icon>
-        </div>
-    </div>
-
-    <div className="curtidas">
-        <img src="assets/img/adorable_animals.svg" />
-        <div className="texto">
-            Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-        </div>
-    </div>
-</div>
-</div> */}
--- src/Sugestoes.js
+++ src/Sugestoes.js
@@ -1,4 +1,28 @@
+function Sugestao(props) {
+    return (
+        <div className="sugestao">
+            <div className="usuario">
+                <img src={props.img} />
+                <div className="texto">
+                    <div className="nome">{props.nome}</div>
+                    <div className="razao">{props.razao}</div>
+                </div>
+            </div>
+
+            <div className="seguir">Seguir</div>
+        </div>
+    )
+
+}
 export default function Sugestoes() {
+
+    const sugestao = [
+        {img:"assets/img/bad.vibes.memes.svg", nome: "bad.vibes.memes", razao: "Segue você"},
+        {img:"assets/img/chibirdart.svg", nome: "chibirdart", razao: "Segue você"},
+        {img:"assets/img/razoesparaacreditar.svg", nome: "razoesparaacreditar", razao: "Novo no Instagram"},
+        {img:"assets/img/adorable_animals.svg", nome: "adorable_animals", razao: "Segue você"},
+        {img: "assets/img/smallcutecats.svg", nome: "smallcutecats", razao: "Segue você"}
+    ]
     return (
         <div className="sugestoes">
             <div className="titulo">
@@ -6,65 +30,8 @@ export default function Sugestoes() {
                 <div>Ver tudo</div>
             </div>
 
-            <div className="sugestao">
-                <div className="usuario">
-                    <img src="assets/img/bad.vibes.memes.svg" />
-                    <div className="texto">
-                        <div className="nome">bad.vibes.memes</div>
-                        <div className="razao">Segue você</div>
-                    </div>
-                </div>
+            {sugestao.map((s) => <Sugestao img={s.img} nome={s.nome} razao={s.razao} />)}
 
-                <div className="seguir">Seguir</div>
-            </div>
-
-            <div className="sugestao">
-                <div className="usuario">
-                    <img src="assets/img/chibirdart.svg" />
-                    <div className="texto">
-                        <div className="nome">chibirdart</div>
-                        <div className="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div className="seguir">Seguir</div>
-            </div>
-
-            <div className="sugestao">
-                <div className="usuario">
-                    <img src="assets/img/razoesparaacreditar.svg" />
-                    <div className="texto">
-                        <div className="nome">razoesparaacreditar</div>
-                        <div className="razao">Novo no Instagram</div>
-                    </div>
-                </div>
-
-                <div className="seguir">Seguir</div>
-            </div>
-
-            <div className="sugestao">
-                <div className="usuario">
-                    <img src="assets/img/adorable_animals.svg" />
-                    <div className="texto">
-                        <div className="nome">adorable_animals</div>
-                        <div className="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div className="seguir">Seguir</div>
-            </div>
-
-            <div className="sugestao">
-                <div className="usuario">
-                    <img src="assets/img/smallcutecats.svg" />
-                    <div className="texto">
-                        <div className="nome">smallcutecats</div>
-                        <div className="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div className="seguir">Seguir</div>
-            </div>
         </div>
 
     )
